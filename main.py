9660386521510c8307e04bd757c9b37edd146c74
ConnorDilloughery <71287859+ConnorDilloughery@users.noreply.github.com>
2023-03-06 10:46:12 -0800

    Push New Files

---
+++ main.py
@@ -0,0 +1,54 @@
+import pyb
+import mlx_cam
+import motorDriver
+state = 0
+
+def main():
+    
+    while True:
+        ## Init
+        if state = 0:
+            pass
+        ## Initial Base Rotation
+        elif state = 1:
+            pass
+        ## Thermal Ideentification
+        elif state = 2:
+            pass
+        ## Top Left Case
+        elif state = 3:
+            pass
+        ## Top Middle Case
+        elif state = 4:
+            pass
+        ## Top Right Case
+        elif state = 5:
+            pass
+        ## Middle Left Case
+        elif state = 6:
+            pass
+        ## Middle Case
+        elif state = 7:
+            pass
+        ## Middle Right Case
+        elif state = 8:
+            pass
+        ## Bottom Left Case
+        elif state = 9:
+            pass
+        ## Bottom Middle Case
+        elif state = 10:
+            pass
+        ## Bottom Right Case
+        elif state = 11:
+            pass
+        ## Activate Gun
+        elif state = 12:
+            pass
+        ## Activate Solenoid / Fire!!!
+        elif state = 13:
+            pass
+        ## DEINIT
+        elif state = 14:
+            print('Complete')
+            # Turn off motor
